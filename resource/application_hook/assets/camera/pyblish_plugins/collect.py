adc7d53d11991b90df4506cf259defcf431b87ad
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-11-07 10:55:16 +0100

    code cleanup

--- resource/application_hook/assets/camera/pyblish_plugins/collect.py
+++ resource/application_hook/assets/camera/pyblish_plugins/collect.py
@@ -2,18 +2,6 @@
 # :copyright: Copyright (c) 2014 ftrack
 
 import pyblish.api
-import ftrack_connect_pipeline.util
-
-
-class FtrackPublishCollector(pyblish.api.ContextPlugin):
-    '''Prepare ftrack publish.'''
-
-    order = pyblish.api.CollectorOrder
-
-    def process(self, context):
-        '''Process *context* and add ftrack entity.'''
-        ftrack_entity = ftrack_connect_pipeline.util.get_ftrack_entity()
-        context.data['ftrack_entity'] = ftrack_entity
 
 
 class CollectCameras(pyblish.api.ContextPlugin):
@@ -34,5 +22,4 @@ class CollectCameras(pyblish.api.ContextPlugin):
                 instance.data['ftrack_components'] = []
 
 
-pyblish.api.register_plugin(FtrackPublishCollector)
 pyblish.api.register_plugin(CollectCameras)
