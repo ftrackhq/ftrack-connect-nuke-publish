e6d242907f79c68a725d2e5c76afa99ed261cb33
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-11-07 10:54:59 +0100

    cleanup old code

--- resource/application_hook/assets/image_sequence/pyblish_plugins/collect.py
+++ resource/application_hook/assets/image_sequence/pyblish_plugins/collect.py
@@ -2,19 +2,7 @@
 # :copyright: Copyright (c) 2016 ftrack
 
 import pyblish.api
-import ftrack_connect_pipeline.util
 import nuke
-
-
-class FtrackPublishCollector(pyblish.api.ContextPlugin):
-    '''Prepare ftrack publish.'''
-
-    order = pyblish.api.CollectorOrder
-
-    def process(self, context):
-        '''Process *context* and add ftrack entity.'''
-        ftrack_entity = ftrack_connect_pipeline.util.get_ftrack_entity()
-        context.data['ftrack_entity'] = ftrack_entity
 
 
 class CollectWriteNodes(pyblish.api.ContextPlugin):
@@ -26,10 +14,11 @@ class CollectWriteNodes(pyblish.api.ContextPlugin):
         '''Process *context* and add nuke write node instances.'''
         for node in nuke.allNodes():
             if node.Class() == 'Write':
-                instance = context.create_instance(node.name(), family='ftrack.nuke.write')
+                instance = context.create_instance(
+                    node.name(), family='ftrack.nuke.write'
+                )
                 instance.data['publish'] = True
                 instance.data['ftrack_components'] = []
 
 
-pyblish.api.register_plugin(FtrackPublishCollector)
 pyblish.api.register_plugin(CollectWriteNodes)
--- resource/application_hook/assets/image_sequence/pyblish_plugins/integrate.py
+++ resource/application_hook/assets/image_sequence/pyblish_plugins/integrate.py
@@ -6,51 +6,6 @@ import nuke
 import tempfile
 
 
-class IntegratorCreateAsset(pyblish.api.ContextPlugin):
-    '''Create asset and prepare publish.'''
-
-    order = pyblish.api.IntegratorOrder
-
-    def process(self, context):
-        '''Process *context* create asset.'''
-
-        ftrack_entity = context.data['ftrack_entity']
-        session = ftrack_entity.session
-
-        asset_type_id = context.data['options']['asset']['asset_type']
-        asset_name = context.data['options']['asset']['asset_name']
-        context_id = ftrack_entity['id']
-
-        asset = session.query(
-            'Asset where context_id is "{0}" and name is "{1}" and '
-            'type_id is "{2}"'.format(
-                context_id, asset_name, asset_type_id
-            )
-        ).first()
-
-        if asset is None:
-            asset = session.create(
-                'Asset',
-                {
-                    'context_id': context_id,
-                    'type_id': asset_type_id,
-                    'name': asset_name
-                }
-            )
-
-        # Create an asset version in a pre-published state.
-        asset_version = session.create(
-            'AssetVersion',
-            {
-                'asset': asset,
-                'is_published': False
-            }
-        )
-
-        session.commit()
-        context.data['asset_version'] = asset_version
-
-
 class IntegratorCreateImageSequenceComponents(pyblish.api.InstancePlugin):
     '''Create ftrack components.'''
 
@@ -66,10 +21,11 @@ class IntegratorCreateImageSequenceComponents(pyblish.api.InstancePlugin):
         asset_version = context.data['asset_version']
         session = asset_version.session
         location = session.pick_location()
-        comment = context.data['options'].get('comment_field', {}).get('comment', 'No comment set')
+        comment = context.data['options'].get(
+            'comment_field', {}
+        ).get('comment', 'No comment set')
 
         for component_item in instance.data.get('ftrack_components', []):
-            component_name = component_item['component_name']
 
             start = int(float(component_item['first']))
             end = int(float(component_item['last']))
@@ -101,7 +57,10 @@ class IntegratorCreateNukeScriptComponent(pyblish.api.ContextPlugin):
 
     def process(self, context):
 
-        comment = context.data['options'].get('comment_field', {}).get('comment', 'No comment set')
+        comment = context.data['options'].get(
+            'comment_field', {}
+        ).get('comment', 'No comment set')
+
         context_options = context.data['options'].get('nuke_media', {})
 
         if context_options.get('attach_nuke_script', False):
@@ -131,22 +90,5 @@ class IntegratorCreateNukeScriptComponent(pyblish.api.ContextPlugin):
             session.commit()
 
 
-class IntegratorPublishVersion(pyblish.api.ContextPlugin):
-    '''Mark asset version as published.'''
-
-    order = pyblish.api.IntegratorOrder + 0.2
-
-    def process(self, context):
-        '''Process *context*.'''
-
-        asset_version = context.data['asset_version']
-        session = asset_version.session
-
-        asset_version['is_published'] = True
-        session.commit()
-
-
-pyblish.api.register_plugin(IntegratorCreateAsset)
 pyblish.api.register_plugin(IntegratorCreateImageSequenceComponents)
 pyblish.api.register_plugin(IntegratorCreateNukeScriptComponent)
-pyblish.api.register_plugin(IntegratorPublishVersion)
