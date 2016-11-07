a3f0e6dcc7940534b5f09ff8eda4aefd200e652e
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-11-07 12:15:25 +0100

    remove dead code

--- resource/application_hook/assets/image_sequence/pyblish_plugins/integrate.py
+++ resource/application_hook/assets/image_sequence/pyblish_plugins/integrate.py
@@ -2,52 +2,7 @@
 # :copyright: Copyright (c) 2016 ftrack
 
 import pyblish.api
-import nuke
 import tempfile
-
-
-# class IntegratorCreateImageSequenceComponents(pyblish.api.InstancePlugin):
-#     '''Create ftrack components.'''
-
-#     order = pyblish.api.IntegratorOrder + 0.1
-
-#     families = ['ftrack.nuke.write']
-
-#     def process(self, instance):
-#         '''Process *instance* and create components.'''
-
-#         import ftrack_api.symbol
-#         context = instance.context
-#         asset_version = context.data['asset_version']
-#         session = asset_version.session
-#         location = session.pick_location()
-#         comment = context.data['options'].get(
-#             'comment_field', {}
-#         ).get('comment', 'No comment set')
-
-#         for component_item in instance.data.get('ftrack_components', []):
-
-#             start = int(float(component_item['first']))
-#             end = int(float(component_item['last']))
-
-#             if start != end:
-#                 sequence_path = u'{0} [{1}-{2}]'.format(
-#                     component_item['file_path'], start, end
-#                 )
-#             else:
-#                 sequence_path = unicode(component_item['file_path'] % start)
-
-#             session.create_component(
-#                 sequence_path,
-#                 {
-#                     'version_id': asset_version['id'],
-#                     'name': component_item['component_name'],
-#                     'comment': comment
-#                 },
-#                 location=location
-#             )
-
-#         session.commit()
 
 
 class IntegratorCreateNukeScriptComponent(pyblish.api.ContextPlugin):
