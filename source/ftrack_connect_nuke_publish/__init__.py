9f3e30cefcdf957e24c7b109e3a3e9e76c447ceb
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-11-08 11:30:15 +0100

    update init for asset registration

--- source/ftrack_connect_nuke_publish/__init__.py
+++ source/ftrack_connect_nuke_publish/__init__.py
@@ -1,4 +1,52 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2016 ftrack
 
+import os
+import functools
+
+import pyblish.plugin
+import ftrack_api.event.base
+
 from ._version import __version__
+
+from ftrack_connect_nuke_publish.asset.camera import camera_asset
+from ftrack_connect_nuke_publish.asset.image_sequence import sequence_asset
+
+
+def register_callback(session, event):
+    '''Handle register event.'''
+    print 'Callback on', session
+    camera_asset.register(session)
+    sequence_asset.register(session)
+
+    path = os.path.normpath(
+        os.path.join(
+            os.path.abspath(
+                os.path.dirname(
+                    __file__
+                )
+            ),
+            'common_pyblish_plugins'
+        )
+    )
+
+    print 'Register plugin path', path
+    pyblish.plugin.register_plugin_path(path)
+
+
+def register_assets(session):
+    '''Register by emitting event on *session*.'''
+    print 'Register listener on session', session
+
+    session.event_hub.subscribe(
+        'topic=ftrack.pipeline.register-assets',
+        functools.partial(register_callback, session)
+    )
+
+    session.event_hub.publish(
+        ftrack_api.event.base.Event(
+            topic='ftrack.pipeline.register-assets',
+            data=dict()
+        ),
+        synchronous=True
+    )
