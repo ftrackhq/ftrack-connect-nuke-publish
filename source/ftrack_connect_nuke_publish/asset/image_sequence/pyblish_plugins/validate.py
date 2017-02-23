dda7f4267070eb7bbf79198436f97ef277610316
Mattias Lagergren <mattias.lagergren@ftrack.com>
2017-02-23 16:16:01 +0100

    Update validation and how sequence is constructed

--- source/ftrack_connect_nuke_publish/asset/image_sequence/pyblish_plugins/validate.py
+++ source/ftrack_connect_nuke_publish/asset/image_sequence/pyblish_plugins/validate.py
@@ -5,19 +5,22 @@ import pyblish.api
 
 
 class ImageSequencePublishValidator(pyblish.api.InstancePlugin):
-    '''Validate Geo publish.'''
+    '''Validate that write node output exists.'''
 
     order = pyblish.api.ValidatorOrder
 
     families = ['ftrack', 'write']
     match = pyblish.api.Subset
 
-    label = 'Validate Image Sequence component.'
+    label = 'Validate that write node output exists.'
+
     optional = False
 
     def check_frames(self, input_frames, write_node, instance_name):
         '''Check that all the frames expressed in *input_frames* exists.
+
         It'll use *write_node* and *instance_name* to fetch data and report.
+
         '''
 
         import nuke
@@ -26,80 +29,65 @@ class ImageSequencePublishValidator(pyblish.api.InstancePlugin):
         import glob
 
         self.log.info(
-            'Validating {0} from {1}'.format(input_frames, instance_name)
+            'Validating {0} from {1}.'.format(input_frames, instance_name)
         )
 
         single_file = os.path.isfile(input_frames)
         if not single_file:
-
-            first = str(int(nuke.root().knob('first_frame').value()))
-            last = str(int(nuke.root().knob('last_frame').value()))
+            first = int(nuke.root().knob('first_frame').value())
+            last = int(nuke.root().knob('last_frame').value())
 
             # Then in case check if the limit are set.
             if write_node['use_limit'].value():
-                first = str(write_node['first'].value())
-                last = str(write_node['last'].value())
+                first = int(write_node['first'].value())
+                last = int(write_node['last'].value())
 
             # Always check how many frames are actually available.
             frames = input_frames
 
-            try:
-                # Try to collect the sequence prefix, padding
-                # and extension. If this fails with a ValueError
-                # we are probably handling a non-sequence file.
-                # If so rely on the first_frame and last_frame
-                # of the root node.
-                prefix, padding, extension = frames.split('.')
-            except ValueError:
-                msg = (
-                    'Could not determine prefix, padding '
-                    'and extension from "{0}".'.format(frames)
-                )
-                self.log.error(msg)
-                assert False, msg
+            fragments = frames.split('.')
 
-            else:
-                root = os.path.dirname(prefix)
-                file_name = os.path.basename(prefix)
-                frame_query = '{0}/{1}*.{2}'.format(
-                    root, file_name, extension
-                )
-                self.log.debug('Looking for {0}'.format(frame_query))
+            assert len(fragments) >= 3, (
+                u'Could not validate that {0} exists.'.format(frames)
+            )
 
-                files = glob.glob(frame_query)
-                collections = clique.assemble(files)
-                frame_collection = collections[0]
-                if len(frame_collection) > 0:
-                    frames = frame_collection[0]
-                else:
-                    assert False, 'frames : {0} has not been not found'.format(
-                        frames
-                    )
+            extension = fragments.pop()
+            # Pop padding, not used.
+            fragments.pop()
+            prefix = '.'.join(fragments)
 
-                for frame in frames:
-                    name, number, ext = frame.split('.')
-                    # check only frames in the current range
-                    frame_path = os.path.join(root, frame)
-                    in_range = int(first) <= int(number) <= int(last)
+            root = os.path.dirname(prefix)
+            file_name = os.path.basename(prefix)
+            frame_query = '{0}/{1}*.{2}'.format(
+                root, file_name, extension
+            )
+            self.log.debug('Looking for {0}'.format(frame_query))
 
-                    if in_range:
-                        assert os.path.exists(
-                            frame_path
-                        ), 'Frame {0} does not exist!'.format(
-                            frame_path
-                        )
-                    else:
-                        self.log.warning(
-                            'Not Checking frame {0} '
-                            'as is out of the current range. {1}-{2}'.format(
-                                frame_path, first, last
-                            )
-                        )
+            files = glob.glob(frame_query)
+            collections, remainder = clique.assemble(files)
+
+            assert collections, u'No frames found: {0}.'.format(frames)
+
+            assert len(collections) == 1, (
+                u'Multiple frame ranges not expected: {0}.'
+            ).format(frames)
+
+            collection = collections[0]
+
+            for index in list(collection.indexes):
+                in_range = first <= index <= last
+                if not in_range:
+                    # Discard index since it will not be published.
+                    collection.indexes.discard(index)
+
+            assert collection.is_contiguous(), (
+                u'Missing images ({0}) in sequence: {1}.'
+            ).format(collection.format('{holes}'), frames)
 
         else:
             assert os.path.exists(
                 input_frames
-            ), 'File {0} does not exist!'.format(input_frames)
+            ), u'File {0} does not exist!'.format(input_frames)
 
     def process(self, instance):
         '''Validate *instance*.'''
