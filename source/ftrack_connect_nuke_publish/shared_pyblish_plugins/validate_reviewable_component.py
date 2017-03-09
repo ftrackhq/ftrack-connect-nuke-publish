54261e490e96a8da2437a5928d333a55552a7f3a
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2017-03-09 14:33:14 +0100

    add validator for write nodes input

---
+++ source/ftrack_connect_nuke_publish/shared_pyblish_plugins/validate_reviewable_component.py
@@ -0,0 +1,34 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2017 ftrack
+
+import pyblish.api
+from ftrack_connect_pipeline import constant
+
+
+class ReviewableComponentValidator(pyblish.api.InstancePlugin):
+    '''Validate exist input to write node.'''
+
+    order = pyblish.api.ValidatorOrder
+
+    families = constant.REVIEW_FAMILY_PYBLISH
+    match = pyblish.api.Subset
+
+    label = (
+        'Validate that write has a valid input'
+        ' to generate a reviewable component.'
+    )
+
+    optional = False
+
+    def process(self, instance):
+        '''Validate *instance*.'''
+        import nuke
+
+        write_node = nuke.toNode(instance.name)
+        input_node = write_node.input(0)
+        assert input_node, (
+            u'No input node found for {0}'.format(instance.name)
+        )
+
+
+pyblish.api.register_plugin(ReviewableComponentValidator)
