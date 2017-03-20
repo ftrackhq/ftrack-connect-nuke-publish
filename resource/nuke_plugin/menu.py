ddb46bd1f560ac93ca59b08f54b51e69b22fda56
Mattias Lagergren <mattias.lagergren@ftrack.com>
2017-03-20 17:06:25 +0100

    Fix issue when publishing in Nuke

--- resource/nuke_plugin/menu.py
+++ resource/nuke_plugin/menu.py
@@ -6,11 +6,11 @@ import os
 import nuke
 import ftrack_connect_pipeline
 
-import ftrack_connect_nuke.plugin
+import ftrack_connect_nuke_publish.plugin
 
 
-plugin = ftrack_connect_nuke.plugin.NukePlugin(
-    contect_id=os.environ['CONTEXT_ID']
+plugin = ftrack_connect_nuke_publish.plugin.NukePlugin(
+    context_id=os.environ['FTRACK_CONTEXT_ID']
 )
 ftrack_connect_pipeline.register_plugin(plugin)
 
@@ -18,4 +18,4 @@ ftrack_connect_pipeline.register_plugin(plugin)
 nuke_menu = nuke.menu('Nuke')
 ftrack_menu = nuke_menu.addMenu('&ftrack new')
 ftrack_menu.addCommand('Publish', plugin.open_publish)
-ftrack_menu.addCommand('Switch Context', plugin.open_change_context)
+ftrack_menu.addCommand('Switch Context', plugin.open_switch_context)
