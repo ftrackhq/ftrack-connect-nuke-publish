7fc25a80fcfa7b9399bcd1d69abda7a67039a796
Mattias Lagergren <mattias.lagergren@ftrack.com>
2017-03-22 11:29:25 +0100

    Update plugin set up

--- resource/nuke_plugin/menu.py
+++ resource/nuke_plugin/menu.py
@@ -5,6 +5,8 @@ import os
 
 import nuke
 import ftrack_connect_pipeline
+import ftrack_connect_pipeline.global_context_switch
+import ftrack_connect_pipeline.publish
 
 import ftrack_connect_nuke_publish.plugin
 
@@ -14,8 +16,17 @@ plugin = ftrack_connect_nuke_publish.plugin.NukePlugin(
 )
 ftrack_connect_pipeline.register_plugin(plugin)
 
+global_context_switch = (
+    ftrack_connect_pipeline.global_context_switch.GlobalContextSwitch(
+        plugin=plugin
+    )
+)
+
+publish = ftrack_connect_pipeline.publish.Publish(
+    plugin=plugin
+)
 
 nuke_menu = nuke.menu('Nuke')
 ftrack_menu = nuke_menu.addMenu('&ftrack new')
-ftrack_menu.addCommand('Publish', plugin.open_publish)
-ftrack_menu.addCommand('Switch Context', plugin.open_switch_context)
+ftrack_menu.addCommand('Publish', publish.open)
+ftrack_menu.addCommand('Switch Context', global_context_switch.open)
