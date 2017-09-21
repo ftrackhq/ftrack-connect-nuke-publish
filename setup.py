972de9c6cbe8e43c37c96edd18fc002ad1adc0fc
Eric Hermelin <erich@cinesite.com>
2017-09-21 14:53:34 +0200

    version up ftrack-connect-pipeline

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ setup(
         (
             'https://bitbucket.org/ftrack/ftrack-connect-pipeline/get/'
             '{version}.zip#egg=ftrack-connect-pipeline-{version}'.format(
-                version='0.8.0'
+                version='0.8.1'
             )
         )
     ],
