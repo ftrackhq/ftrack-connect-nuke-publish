6893cbfa5700bcf21c8cc22a8a0bb2861de87047
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-11-05 09:16:53 +0100

    pep8 cleanup

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ import os
 import re
 import shutil
 
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import setuptools
-from pkg_resources import parse_version
-
 import pip
+import setuptools
+from pip._internal import main as pip_main
+from pkg_resources import parse_version
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
 
 if parse_version(pip.__version__) < parse_version('19.3.0'):
     raise ValueError('Pip should be version 19.3.0 or higher')
 
-from pip._internal import main as pip_main
 
 FTRACK_CONNECT_PIPELINE_VERSION = '0.8.4'
 
@@ -51,6 +50,7 @@ with open(os.path.join(
 
 # Update staging path with the plugin version
 STAGING_PATH = STAGING_PATH.format(VERSION)
+
 
 # Custom commands.
 class PyTest(TestCommand):
@@ -117,6 +117,7 @@ class BuildPlugin(setuptools.Command):
 
         print 'Result: ' + result_path
 
+
 # Configuration.
 setup(
     name='ftrack-connect-nuke-publish',
@@ -140,8 +141,15 @@ setup(
     install_requires=[
         'clique==1.3.1',
         'pyblish-base >= 1.4.3',
-        'ftrack-connect-pipeline @ https://bitbucket.org/ftrack/ftrack-connect-pipeline/get/{0}.zip#egg=ftrack-connect-pipeline-{0}'.format(FTRACK_CONNECT_PIPELINE_VERSION),
-        'qtext @ git+https://bitbucket.org/ftrack/qtext/get/0.2.2.zip#egg=QtExt-0.2.2'
+        (
+            'ftrack-connect-pipeline @ https://bitbucket.org/ftrack/'
+            'ftrack-connect-pipeline/get/{0}.zip'
+            '#egg=ftrack-connect-pipeline-{0}'
+        ).format(FTRACK_CONNECT_PIPELINE_VERSION),
+        (
+            'qtext @ git+https://bitbucket.org/ftrack/qtext/get/0.2.2.zip'
+            '#egg=QtExt-0.2.2'
+        )
     ],
     tests_require=[
         'pytest >= 2.3.5, < 3'
